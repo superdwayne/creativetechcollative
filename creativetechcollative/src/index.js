376cc28bf525f6c270cfb3c37c594702db1f4ade
Dwayne Paisley-Marshall <superdwayne@gmail.com>
2023-12-02 20:30:41 +0100

    added analytics

--- creativetechcollative/src/index.js
+++ creativetechcollative/src/index.js
@@ -4,10 +4,20 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-S5057CG8DS"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments)}
+  gtag('js', new Date());
+  gtag('config', 'G-S5057CG8DS');
+</script>
     <App />
+  
   </React.StrictMode>
 );
 
