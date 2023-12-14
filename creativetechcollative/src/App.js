a355c137b54af2b28ab93105ab3561cdb1294b19
Dwayne Paisley-Marshall <superdwayne@gmail.com>
2023-12-14 11:43:37 +0100

    Added new copy

--- creativetechcollative/src/App.js
+++ creativetechcollative/src/App.js
@@ -7,18 +7,18 @@ import './App.css';
 const TRACKING_ID = "UA-294935751-1"; // OUR_TRACKING_ID
 ReactGA.initialize(TRACKING_ID);
 
-const AboutOverlay = ({ onClose }) => {
-  return (
-    <div className="about-overlay">
+// const AboutOverlay = ({ onClose }) => {
+//   return (
+//     <div className="about-overlay">
       
-      <div className='aboutcontent'>
-      <button className="close-button" onClick={onClose}>X</button>
-        <h2>At the Creative Technology Collective, we are a dynamic team of eight creative technologists, blending art and technology to redefine digital expression. Our diverse skills range from software engineering and digital artistry to interactive media and beyond. United by a philosophy of collaborative intelligence, each of us brings unique insights and expertise, fueling innovation and pioneering concepts. </h2>
-        <h2>We hold monthly meetups to discuss the latest in the world of creative tech, fostering a space for learning, sharing, and sparking new ideas. Our mission extends beyond creating groundbreaking technology; we aim to inspire and share our collective wisdom with the community, driving forward the future of digital creativity.</h2>
-      </div>     
-    </div>
-  );
-};
+//       <div className='aboutcontent'>
+//       <button className="close-button" onClick={onClose}>X</button>
+//         <h2>At the Creative Technology Collective, we are a dynamic team of eight creative technologists, blending art and technology to redefine digital expression. Our diverse skills range from software engineering and digital artistry to interactive media and beyond. United by a philosophy of collaborative intelligence, each of us brings unique insights and expertise, fueling innovation and pioneering concepts. </h2>
+//         <h2>We hold monthly meetups to discuss the latest in the world of creative tech, fostering a space for learning, sharing, and sparking new ideas. Our mission extends beyond creating groundbreaking technology; we aim to inspire and share our collective wisdom with the community, driving forward the future of digital creativity.</h2>
+//       </div>     
+//     </div>
+//   );
+// };
 
 const Weather = ({ onAboutClick }) => {
   const [weather, setWeather] = useState(null);
@@ -46,9 +46,8 @@ const Weather = ({ onAboutClick }) => {
 
   return (
     <nav className="navbar">
-      <div className="nav-brand">  {weather.main.temp}°C Amsterdam </div>
+      <div className="nav-brand"> Creative Technology Collective  </div>
       <div className="nav-links">
-        <a href="/about" onClick={onAboutClick}>About</a>
         <a target="_blank" href="mailto:ddpmarshall@gmail.com?subject=I want to support">
         Support us
         </a>
@@ -59,21 +58,10 @@ const Weather = ({ onAboutClick }) => {
 };
 
 function App() {
-  const [showAbout, setShowAbout] = useState(false);
-
-  const handleAboutClick = (event) => {
-    event.preventDefault();
-    setShowAbout(true);
-  };
-
-  const handleCloseOverlay = () => {
-    setShowAbout(false);
-  };
-
+ 
   return (
     <>
-      <Weather onAboutClick={handleAboutClick} />
-      {showAbout && <AboutOverlay onClose={handleCloseOverlay} />}
+      <Weather />
       <TeamPage />
     </>
   );
--- creativetechcollative/src/team/team.js
+++ creativetechcollative/src/team/team.js
@@ -1,18 +1,14 @@
 import React from "react";
 import "./team.css";
 const teamMembers = [
-  {
-    name: "Angelica Ortiz",
-    title: "Senior Creative Technologist",
-    image: "./images/4.png",
-    linkedin: "https://www.linkedin.com/in/angelicaortiz",
-  },
-  {
-    name: "Jennifer Cohen",
-    title: "Senior Creative Technologist",
-    image: "./images/5.png",
-    linkedin: "https://www.linkedin.com/in/jennifermcohen/",
-  },
+    {
+        name: "Dwayne Paisley-Marshall",
+        title: "Senior Creative Technologist",
+        image: "./images/7.png",
+        linkedin: "https://www.linkedin.com/in/ddpmarshall/",
+    
+      },
+  
   {
     name: "James Milton",
     title: "Creative Technologist",
@@ -25,12 +21,22 @@ const teamMembers = [
     image: "./images/2.png",
     linkedin: "https://www.linkedin.com/in/pax-man-b0173047/",
   },
+  
+  
   {
-    name: "Koen Mostert",
+    name: "Jennifer Cohen",
     title: "Senior Creative Technologist",
-    image: "./images/3.png",
-    linkedin: "https://www.linkedin.com/in/koenmostert",
+    image: "./images/5.png",
+    linkedin: "https://www.linkedin.com/in/jennifermcohen/",
   },
+  {
+    name: "Angelica Ortiz",
+    title: "Senior Creative Technologist",
+    image: "./images/4.png",
+    linkedin: "https://www.linkedin.com/in/angelicaortiz",
+  },
+  
+  
   {
     name: "Deniz Kurt",
     title: "Creative Technologist",
@@ -38,12 +44,12 @@ const teamMembers = [
     linkedin: "https://www.linkedin.com/in/denizezgikurt/",
   },
   {
-    name: "Dwayne Paisley-Marshall",
+    name: "Koen Mostert",
     title: "Senior Creative Technologist",
-    image: "./images/7.png",
-    linkedin: "https://www.linkedin.com/in/ddpmarshall/",
-
+    image: "./images/3.png",
+    linkedin: "https://www.linkedin.com/in/koenmostert",
   },
+ 
 ];
 
 const TeamMember = ({ name, title, image, linkedin }) => (
@@ -63,29 +69,64 @@ const TeamMember = ({ name, title, image, linkedin }) => (
 
 const TeamPage = () => (
   <div className="team-container">
-    <div className="header">
-      <h1>
-        We are a diverse collective of creative technologists dedicated to
-        pushing the boundaries of what's possible
-      </h1>
-    </div>
-
    
+
+    <div className="grid-container">
+      <div className="flex-row">
+        <div className="flex-cell">
+          {" "}
+          <h1>
+            The Creative Technology Collective is a dynamic team of creative
+            technologists blending art and tech to redefine creative expression.
+          </h1>
+        </div>
+        <div className="flex-cell">
+          {" "}
+          <h3>
+            United by a philosophy of collaborative intelligence, each of us
+            brings unique insights and expertise, fueling innovation and
+            pioneering concepts.
+          </h3>
+        </div>
+      </div>
+      <div className="flex-row">
+        <div className="flex-cell">
+          {" "}
+          <h3>
+            Our mission extends beyond creating groundbreaking technology; we
+            aim to inspire and share our collective wisdom with the community,
+            driving forward the future of creativity and technology.
+          </h3>
+        </div>
+        <div className="flex-cell">
+          {" "}
+          <h1>
+            We hold meetups to discuss the latest in the world of creative tech,
+            fostering a space for learning, sharing, and sparking new ideas.
+          </h1>
+        </div>
+      </div>
+    </div>
 
     <div className="team-grid">
       <div className="visionaries">
         <p>CREATIVE TECH COLLECTIVE</p>
-        <h2>Meet the <br /> visionaries</h2>
+        <h2>
+          Members
+        </h2>
       </div>
       {teamMembers.map((member, index) => (
         <TeamMember key={index} {...member} />
       ))}
       <div className="meetup-invite">
-      <a target="_blank" href="mailto:ddpmarshall@gmail.com?subject=Hosting%20the%20Next%20Meetup">
-    <button>Want to host our next meet up?</button>
-</a>    </div>
+        <a
+          target="_blank"
+          href="mailto:ddpmarshall@gmail.com?subject=Hosting%20the%20Next%20Meetup"
+        >
+          <button>Want to host our next meet up?</button>
+        </a>{" "}
+      </div>
     </div>
-    
   </div>
 );
 
